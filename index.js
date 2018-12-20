67117fa5ab9a7f9030edb9bb007dd2feffbbc114
Marrio Hinkle <kalunga212@gmail.com>
2018-12-20 15:50:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var musicianString;
-  var musicaianInstrument=[]
+  var musicaianInstrument=[];
   for(var i=0;i<musicians.length; i++){
     musicianString= musicians[i]+ 'plays' + instruments[i];
     musiciansInstrument[i]=musicianString;
